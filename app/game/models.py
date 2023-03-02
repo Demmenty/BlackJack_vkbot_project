74f02078560a9f54a38b1319ccf7b3ac6eb1aa50
Demmenty <Demmenty@gmail.com>
2023-03-02 21:05:21 +0400

    добавлено поле cash в модель Player

--- app/game/models.py
+++ app/game/models.py
@@ -54,6 +54,7 @@ class PlayerModel(db):
     id = Column(Integer, primary_key=True)
     user_id = Column(ForeignKey("vk_user.id"), nullable=False)
     game_id = Column(ForeignKey("game.id", ondelete="CASCADE"), nullable=False)
+    cash = Column(Integer, default=1000, nullable=False)
 
     game = relationship("GameModel", back_populates="players")
     vk_user = relationship("VKUserModel", back_populates="players")
