f707da225ff6f86c6f21a35d18942e1c1d2f5980
Demmenty <Demmenty@gmail.com>
2023-03-05 18:54:15 +0400

    декораторы для проверки необх. состояния игры

---
+++ app/store/game/state_checkers.py
@@ -0,0 +1,62 @@
+import typing
+
+from app.store.vk_api.dataclasses import Update
+
+if typing.TYPE_CHECKING:
+    from app.store.game.manager import GameManager
+
+
+# Как мне лучше этот файл назвать?.. utils, decorators ..
+
+
+def game_must_be_on(method):
+    """если активной игры в этом чате не ведется -
+    отменяет выполнение метода и посылает соответствующее уведомление"""
+
+    async def wrapper(self: "GameManager", update: Update, *args, **kwargs):
+        game_is_on = await self.app.store.game.is_game_on(update.peer_id)
+
+        if not game_is_on:
+            await self.notifier.game_is_off(peer_id=update.peer_id)
+            return
+
+        return await method(self, update, *args, **kwargs)
+
+    return wrapper
+
+
+def game_must_be_off(method):
+    """если в чате ведется игра - отменяет выполнение метода"""
+
+    async def wrapper(self: "GameManager", update: Update, *args, **kwargs):
+        game_is_on = await self.app.store.game.is_game_on(update.peer_id)
+
+        if game_is_on:
+            return
+
+        return await method(self, update, *args, **kwargs)
+
+    return wrapper
+
+
+def game_must_be_on_state(*states):
+    """если игра не на одной из переданных стадий - отменяет выполнение метода
+    и посылает соответствующее уведомление"""
+
+    def decorator(method):
+        async def wrapper(self: "GameManager", update: Update, *args, **kwargs):
+            game = await self.app.store.game.get_game_by_peer_id(update.peer_id)
+
+            if game.state not in states:
+                await self.notifier.wrong_state(peer_id=update.peer_id)
+                return
+
+            return await method(self, update, *args, **kwargs)
+
+        return wrapper
+
+    return decorator
+
+
+# TODO @user_must_be_a_player
+# хм, а декораторов бывает много?
