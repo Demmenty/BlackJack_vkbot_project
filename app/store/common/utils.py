c5f24940c042f43624c7a2a0734f18374f521ee1
Demmenty <Demmenty@gmail.com>
2023-03-06 10:24:42 +0400

    + cool func get_noun_ending

---
+++ app/store/common/utils.py
@@ -0,0 +1,15 @@
+def get_noun_ending(number, one, two, five) -> str:
+    """возвращает вариант слова с правильным окончанием в зависимости от числа
+    Нужно передать число и соответствующие варианты
+    например: get_noun_ending(4, 'слон', 'слона', 'слонов'))
+    """
+    n = abs(number)
+    n %= 100
+    if 20 >= n >= 5:
+        return five
+    n %= 10
+    if n == 1:
+        return one
+    if 4 >= n >= 2:
+        return two
+    return five
