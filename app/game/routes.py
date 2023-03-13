30e129231a5d918ad87d0cac9419c1f06e9eba0c
Demmenty <Demmenty@gmail.com>
2023-03-13 18:30:13 +0400

    регистрация путей

--- app/game/routes.py
+++ app/game/routes.py
@@ -1,7 +1,10 @@
-# import typing
-# if typing.TYPE_CHECKING:
-#     from app.web.app import Application
+import typing
+
+if typing.TYPE_CHECKING:
+    from app.web.app import Application
 
 
-# def setup_routes(app: "Application"):
-# app.router.add_view("/game.smth", SomeView)
+def setup_routes(app: "Application"):
+    from app.game.views import ChatStatView
+
+    app.router.add_view("/game.stat", ChatStatView)
