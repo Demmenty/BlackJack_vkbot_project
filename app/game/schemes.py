8b609dfb996f01870270bd0add63ff8695b8bbd9
Demmenty <Demmenty@gmail.com>
2023-03-13 19:12:03 +0400

    + схемы ответов и запросов статистики пользователя

--- app/game/schemes.py
+++ app/game/schemes.py
@@ -8,3 +8,20 @@ class ChatStatRequestSchema(Schema):
 class ChatStatResponseSchema(Schema):
     games_played = fields.Int(required=True)
     casino_cash = fields.Int(required=True)
+
+
+class UserStatRequestSchema(Schema):
+    user_id = fields.Int(required=True)
+
+
+class UserStatResponseSchema(Schema):
+    user_id = fields.Int(required=True)
+    number_of_gamechats = fields.Int(required=True)
+    player_stat = fields.Nested("PlayerStatSchema", many=True)
+
+
+class PlayerStatSchema(Schema):
+    game_id = fields.Int(required=True)
+    games_played = fields.Int(required=True)
+    games_won = fields.Int(required=True)
+    games_lost = fields.Int(required=True)
