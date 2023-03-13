0e0df44454da65edf0beeb51ace4e6cf2473498e
Demmenty <Demmenty@gmail.com>
2023-03-13 18:30:54 +0400

    + схемы для запросов и ответов

--- app/game/schemes.py
+++ app/game/schemes.py
@@ -1,1 +1,10 @@
-# from marshmallow import Schema, fields
+from marshmallow import Schema, fields
+
+
+class ChatStatRequestSchema(Schema):
+    chat_id = fields.Int(required=True)
+
+
+class ChatStatResponseSchema(Schema):
+    games_played = fields.Int(required=True)
+    casino_cash = fields.Int(required=True)
