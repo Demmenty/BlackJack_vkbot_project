81185bcf6e11f201de248ab0cd77ed289d6747c1
Demmenty <Demmenty@gmail.com>
2023-03-13 19:14:03 +0400

    + user statistic view

--- app/game/views.py
+++ app/game/views.py
@@ -1,7 +1,13 @@
 from aiohttp.web import HTTPNotFound
 from aiohttp_apispec import docs, request_schema, response_schema
 
-from app.game.schemes import ChatStatRequestSchema, ChatStatResponseSchema
+from app.game.schemes import (
+    ChatStatRequestSchema,
+    ChatStatResponseSchema,
+    PlayerStatSchema,
+    UserStatRequestSchema,
+    UserStatResponseSchema,
+)
 from app.web.app import View
 from app.web.mixins import AuthRequiredMixin
 from app.web.utils import json_response
@@ -11,7 +17,7 @@ class ChatStatView(AuthRequiredMixin, View):
     @docs(
         tags=["chat_stat"],
         summary="get chat statistic",
-        description="get chat statistics about the number of games played and the bank of the casino by vk chat id",
+        description="get chat statistic about the number of games played and the bank of the casino by vk chat id",
     )
     @request_schema(ChatStatRequestSchema)
     @response_schema(ChatStatResponseSchema, 200)
@@ -28,5 +34,33 @@ class ChatStatView(AuthRequiredMixin, View):
         data = {
             "games_played": chat.games_played,
             "casino_cash": chat.casino_cash,
+        }
+        return json_response(data)
+
+
+class UserStatView(AuthRequiredMixin, View):
+    @docs(
+        tags=["user_stat"],
+        summary="get user statistic",
+        description="get user statistic about the number of games he played, won and lost as player",
+    )
+    @request_schema(UserStatRequestSchema)
+    @response_schema(UserStatResponseSchema, 200)
+    async def get(self):
+        request_user_id = self.request["data"]["user_id"]
+
+        players = await self.request.app.store.game.get_players_of_user(
+            request_user_id
+        )
+
+        if not players:
+            raise HTTPNotFound(reason="user is not registered as a player")
+
+        player_stat = [PlayerStatSchema().dump(player) for player in players]
+
+        data = {
+            "user_id": request_user_id,
+            "number_of_gamechats": len(players),
+            "player_stat": player_stat,
         }
         return json_response(data)
